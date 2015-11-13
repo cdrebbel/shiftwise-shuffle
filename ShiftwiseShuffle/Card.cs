301fcb82bb6353b803fa9a01186593104dc94f68
cdrebbel <cdrebbel@gmail.com>
2015-11-13 21:45:44 +0000

    Initial commit with first compare implementation.

---
+++ ShiftwiseShuffle/Card.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShiftwiseShuffle
+{
+    public class Card : IComparable
+    {
+        public int FaceValue { get; set; }
+
+        public int CompareTo(object obj)
+        {
+            var card = (Card)obj;
+            return FaceValue >= card.FaceValue ? 1 : -1;
+        }
+    }
+}
---
+++ ShiftwiseShuffle/SortTests.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using FluentAssertions;
+
+namespace ShiftwiseShuffle
+{
+    [TestFixture]
+    public class SortTests
+    {
+        [Test]
+        public void TwoIsGreaterThanOne()
+        {
+            GivenDeck();
+            WhenDeckIsSorted();
+            ThenCardsAreInOrder();
+
+        }
+
+        private List<Card> _deck; 
+
+        private void GivenDeck()
+        {
+            _deck = new List<Card>();
+            _deck.Add(new Card() { FaceValue = 13 });
+            _deck.Add(new Card() { FaceValue = 1 });
+            _deck.Add(new Card() { FaceValue = 7 });
+        }
+
+        public void WhenDeckIsSorted()
+        {
+            _deck.Sort();
+        }
+
+        private void ThenCardsAreInOrder()
+        {
+            var prevCard= _deck.First();
+            _deck.Remove(prevCard);
+            foreach(var card in _deck)
+            {
+                prevCard.FaceValue.Should().BeLessOrEqualTo(card.FaceValue);
+            }
+        }
+    }
+}
