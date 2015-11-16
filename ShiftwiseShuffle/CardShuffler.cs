434eba1d13fc415bc05c0e0c0f876b6765f83f74
cdrebbel <cdrebbel@gmail.com>
2015-11-16 20:50:28 +0000

    Added Shuffler. It's very simple, as it goes through each place in the deck and picks a random card elsewhere to swap with. The test does have a chance of failing, but it is very low.

---
+++ ShiftwiseShuffle/CardShuffler.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ShiftwiseShuffle
+{
+    public class CardShuffler
+    {
+        private List<Card> _deck;
+
+        public CardShuffler(List<Card> deck)
+        {
+            _deck = deck;
+        }
+
+        public List<Card> Shuffle()
+        {
+            var rand = new Random();
+            for (int i = 0; i < _deck.Count; i++)
+            {
+                var moveMe = _deck[i];
+                var fromHere = rand.Next() % _deck.Count;
+
+                _deck[i] = _deck[fromHere];
+                _deck[fromHere] = moveMe;
+            }
+
+            return _deck;
+        }
+    }
+}
---
+++ ShiftwiseShuffle/ShuffleTests.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework;
+using FluentAssertions;
+
+namespace ShiftwiseShuffle
+{
+    [TestFixture]
+    public class ShuffleTests
+    {
+        private CardShuffler _shuffler;
+        private List<Card> _deck;
+        private List<Card> _shuffledDeck;
+
+        [Test]
+        public void ShouldComeBackInDifferentOrder()
+        {
+            GivenDeck();
+            GivenShuffler();
+            WhenShuffled();
+            ThenInDifferentOrder();
+        }
+
+        private void GivenDeck()
+        {
+            _deck = new List<Card>();
+
+            //Adding lots of cards to reduce chance that test fails.
+            for(int i = 1; i <= 100; i++){
+                _deck.Add(new Card() { FaceValue = i });
+            };
+        }
+
+        private void GivenShuffler()
+        {
+            _shuffler = new CardShuffler(_deck.Select(c=>c).ToList()); //Making a copy of the list, so we can do a comparison
+        }
+
+        private void WhenShuffled()
+        {
+            //Could be convinced to pass _deck in here rather than constructor. Didn't want to overthink it.
+            _shuffledDeck = _shuffler.Shuffle();
+        }
+
+        private void ThenInDifferentOrder()
+        {
+            var diffCount = 0;
+            for (int i = 0; i < _deck.Count; i++)
+            {
+                if(_deck[i] != _shuffledDeck[i])
+                {
+                    diffCount++;
+                }
+            }
+
+            diffCount.Should().BeGreaterThan(0);
+        }
+    }
+}
